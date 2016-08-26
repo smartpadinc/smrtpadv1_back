441a644cc14edfcac3da8dbdd68ac0363963b5e2
Leo Diaz <leoangelo.diaz@gmail.com>
2016-08-26 17:29:32 +0000

    add template support

--- smrtpadv1_back/settings.py
+++ smrtpadv1_back/settings.py
@@ -123,4 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "assets/"),
+# ]
+STATIC_URL = '/assets/'
