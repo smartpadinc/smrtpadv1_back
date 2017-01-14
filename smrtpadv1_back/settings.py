4c4b86ceea29a0fcb72fd3c24b0ed02765fd29a7
Leo Diaz <leoangelo.diaz@gmail.com>
2017-01-14 14:40:24 +0000

    change dir of env

--- smrtpadv1_back/settings.py
+++ smrtpadv1_back/settings.py
@@ -13,12 +13,14 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import envvars
 
-envvars.load('/var/www/.env')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+
+envvars.load(BASE_DIR + '/.env')
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
