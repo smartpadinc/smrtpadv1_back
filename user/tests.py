352b4104b5d78dc802a5f70f7cc3bb25f56fd788
Leo Diaz <leoangelo.diaz@gmail.com>
2017-02-11 18:11:02 +0000

    update test

--- user/tests.py
+++ user/tests.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase, APIClient, APIRequestFactory
 
 from django.contrib.auth.models import User
-
+from user import models as usermod
 # Create your tests here.
 
 class UserManagementTestCase(TestCase):
@@ -19,8 +19,8 @@ class UserManagementTestCase(TestCase):
         token.save()
 
         self.url = {
-            'user'                  : '/api/user/account/',
-            'user_change_password'  : '/api/user/account/change_password/',
+            'user'                  : '/api/user/account',
+            'user_change_password'  : '/api/user/change_password/',
             'user_profile'          : '/api/user/profile/',
             'organization'          : '/api/user/organization/',
         }
@@ -34,7 +34,7 @@ class UserManagementTestCase(TestCase):
 
     def test_get_user_account(self):
         self.client.login(username='testuser', password='testing')
-        response = self.client.get(self.url['user'])
+        response = self.client.get(self.url['user'] + "//")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_add_new_valid_account_authorized(self):
@@ -45,8 +45,9 @@ class UserManagementTestCase(TestCase):
         self.data['user_type']  = 2
 
         response = self.client.post(self.url['user'], self.data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['success'], 'true')
+        self.assertEqual(response.data['success'], True)
 
     def test_ensure_correct_field_value_onsave(self):
         self.client.login(username='testuser', password='testing')
@@ -62,14 +63,14 @@ class UserManagementTestCase(TestCase):
         self.assertEqual(rd['email'], self.data['email'])
         self.assertEqual(rd['first_name'], self.data['first_name'])
         self.assertEqual(rd['last_name'], self.data['last_name'])
-        self.assertEqual(response.data['success'], 'true')
+        self.assertEqual(response.data['success'], True)
 
-    def test_add_new_valid_account_unauthorized(self):
-        self.data['username']   = 'Test2'
-        self.data['email']      = 'leoangelo.dia123@gmail.com'
-
-        response = self.client.post(self.url['user'], self.data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    # def test_add_new_valid_account_unauthorized(self):
+    #     self.data['username']   = 'Test2'
+    #     self.data['email']      = 'leoangelo.dia123@gmail.com'
+    #
+    #     response = self.client.post(self.url['user'], self.data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_add_new_valid_account_duplicate_email(self):
         self.client.login(username='testuser', password='testing')
@@ -84,7 +85,7 @@ class UserManagementTestCase(TestCase):
 
         response = self.client.post(self.url['user'], self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['success'], 'false')
+        self.assertEqual(response.data['success'], False)
 
     def test_add_new_valid_account_duplicate_username(self):
         self.client.login(username='testuser', password='testing')
@@ -99,7 +100,7 @@ class UserManagementTestCase(TestCase):
 
         response = self.client.post(self.url['user'], self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['success'], 'false')
+        self.assertEqual(response.data['success'], False)
 
     def test_add_new_invalid_account_authorized(self):
         self.client.login(username='testuser', password='testing')
@@ -109,52 +110,79 @@ class UserManagementTestCase(TestCase):
 
         response = self.client.post(self.url['user'], self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['success'], 'false')
+        self.assertEqual(response.data['success'], False)
 
     def test_change_password(self):
         self.client.login(username='testuser', password='testing')
 
         data = {
-            "old_password" : 'testing',
+            "password" : 'testing',
             "new_password" : 'demo1234',
         }
 
-        response = self.client.patch(self.url['user_change_password'], data, format='json')
+        response = self.client.post(self.url['user_change_password'] + str(self.user.id) + "/", data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['success'], 'true')
+        self.assertEqual(response.data['success'], True)
 
     def test_change_invalid_password(self):
         self.client.login(username='testuser', password='testing')
 
         data = {
-            "old_password" : 'testing123',
+            "password" : 'testing123',
             "new_password" : 'demo1234',
         }
 
-        response = self.client.patch(self.url['user_change_password'], data, format='json')
+        response = self.client.post(self.url['user_change_password'] + "{}/".format(str(self.user.id)), data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['success'], 'false')
+        self.assertEqual(response.data['success'], False)
+
+    def test_change_password_using_invalid_id(self):
+        self.client.login(username='testuser', password='testing')
+
+        id = 9999
+        data = {
+            "password" : 'testing123',
+            "new_password" : 'demo1234',
+        }
+
+        response = self.client.post(self.url['user_change_password'] + "{}/".format(str(id)), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['success'], False)
+
+    def test_change_password_using_incomplete_params(self):
+        self.client.login(username='testuser', password='testing')
+
+        id = 9999
+        data = {}
+
+        response = self.client.post(self.url['user_change_password'] + "{}/".format(str(id)), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['success'], False)
 
     def test_get_user_profile(self):
         self.client.login(username='testuser', password='testing')
-        response = self.client.get(self.url['user_profile'])
+        response = self.client.get(self.url['user'] + '/{}/'.format(str(self.user.id)) )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-    def test_add_new_profile(self):
+    def test_update_profile(self):
         self.client.login(username='testuser', password='testing')
 
-        self.data['username']   = 'Test'
-        self.data['email']      = 'leoangelo.dia123@gmail.com'
         self.data['first_name'] = 'Leo'
         self.data['last_name']  = 'Diaz'
-        self.data['user_type']  = 2
-        self.client.post(self.url['user'], self.data, format='json')
 
-        response = self.client.get(self.url['user_profile'])
+        profile = usermod.UserProfile()
+        profile.user_id     = self.user.id
+        profile.user_type   = 1
+        profile.first_name  = 'Leo'
+        profile.last_name   = 'Diaz'
+        profile.save()
+
+        response = self.client.patch(self.url['user_profile'] + "{}/".format(str(self.user.id)), self.data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_organization(self):
-        self.client.login(username='testuser', password='testing')
-        response = self.client.get(self.url['organization'])
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    # def test_organization(self):
+    #     self.client.login(username='testuser', password='testing')
+    #     response = self.client.get(self.url['organization'])
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
