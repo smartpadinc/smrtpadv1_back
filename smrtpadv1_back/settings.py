68f7109005eec7452b2e2aa5888366d1ac7b08a4
Leo Diaz <leoangelo.diaz@gmail.com>
2017-03-05 07:44:33 +0000

    add ACCESS_TOKEN_EXPIRE in settings

--- smrtpadv1_back/settings.py
+++ smrtpadv1_back/settings.py
@@ -51,6 +51,7 @@ OAUTH2_PROVIDER = {
     # this is the list of available scopes
     'SCOPES': {'read': 'Read scope', 'write': 'Write scope'},
     #'OAUTH2_BACKEND_CLASS': 'oauth2_provider.oauth2_backends.JSONOAuthLibCore'
+    'ACCESS_TOKEN_EXPIRE_SECONDS': 1800 #30minutes
 }
 
 
