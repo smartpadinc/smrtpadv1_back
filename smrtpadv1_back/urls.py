ddaaa9756701e0d5d80d42c425610ff9018d0526
Leo Diaz <leoangelo.diaz@gmail.com>
2017-03-25 18:04:09 +0000

    fix url conflict

--- smrtpadv1_back/urls.py
+++ smrtpadv1_back/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     url(r'^$', include('utils.urls', namespace='baseview')),
     url(r'^admin/', admin.site.urls),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    #v1
     url(r'^api/user/', include('user.urls')),
     url(r'^api/auth/', include('smrt_auth.urls')),
     #url(r'^api/properties/', include('properties.urls')),
     url(r'^docs/', include('rest_framework_docs.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
