c313ebfc59a60f19f380d9759d41bcde3419197a
Leo Diaz <leoangelo.diaz@gmail.com>
2017-03-25 18:03:56 +0000

    send email class (ongoing)

---
+++ utils/email.py
@@ -0,0 +1,12 @@
+from templated_email import send_templated_mail
+
+class SendEmail:
+
+    def displayCount(name):
+        print(name)
+
+        # send_templated_mail(template_name='welcome',
+        #     from_email='SmartPad <notifications@smartpad.ph>',
+        #     recipient_list=['olivelaw.diaz@gmail.com','leo@crazyapp.cloud'],
+        #     context={'full_name': "OLIVE", "username": "olive.diaz"}
+        # )
